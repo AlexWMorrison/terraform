a69563f2f69457aad36fd2d19b35ec50afca1324
AMorris1 <amorris1@scgcontractor.com>
2022-03-01 13:32:26 -0800

    Update resource location for cdn profile

--- azure/modules/cdn_profile/module.tf
+++ azure/modules/cdn_profile/module.tf
@@ -5,7 +5,7 @@ data "azurerm_resource_group" "cdnRG" {
 resource "azurerm_cdn_profile" "cdnProfile" {
     name = var.cdn_profile_name
     resource_group_name = data.azurerm_resource_group.cdnRG.name
-    location = data.azurerm_resource_group.cdnRG.name
+    location = data.azurerm_resource_group.cdnRG.location
     sku = var.settings.sku
     #tags = local.tags
 }
